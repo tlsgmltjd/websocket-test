738ce50138fb69e271f933c15e8c86b30f161658
신희성 <vvwv3092@gmail.com>
2024-06-04 16:09:36 +0900

    init

---
+++ src/main/java/com/example/wstest/WebSocketAuthIntercepter.java
@@ -0,0 +1,50 @@
+package com.example.wstest;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WebSocketAuthIntercepter implements HandshakeInterceptor {
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        if (request instanceof ServletServerHttpRequest) {
+            HttpServletRequest servletRequest = ((ServletServerHttpRequest) request).getServletRequest();
+            HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+
+            Cookie[] cookies = servletRequest.getCookies();
+
+            if (cookies == null) {
+                log.error("쿠키가 없습니다.");
+                servletResponse.setStatus(401);
+                return false;
+            } else {
+                for (Cookie cookie : cookies) {
+                    log.info("================================ " + cookie.getName() + " : " + cookie.getValue());
+                }
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
---
+++ src/test/java/com/example/wstest/WstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
